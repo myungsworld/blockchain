3afcaceabbcc9fd07d4bf8e1410a17cae1c610d1
myungsworld <myungsworld@gmail.com>
2020-07-03 15:39:29 +0900

    updated Mining

--- main.js
+++ main.js
@@ -6,16 +6,25 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash= this.calculateHash();
+        this.hash = this.calculateHash();
+        this.nonce = 0;
     }
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block minded " + this.hash);
     }
 }
 
 class BlockChain {
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -28,7 +37,7 @@ class BlockChain {
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -51,15 +60,10 @@ class BlockChain {
 }
 
 let myungsCoin = new BlockChain();
+
+console.log('Mining Block 1...')
 myungsCoin.addBlock(new Block(1, "07/03/2020",{amount:4}));
+console.log('Mining Block 2...')
 myungsCoin.addBlock(new Block(2, "07/03/2020",{amount:10}));
 
 
-console.log('is blockchain valid?',myungsCoin.isChainValid());
-myungsCoin.chain[1].data = {amount:200};
-myungsCoin.chain[1].hash = myungsCoin.chain[1].calculateHash();
-console.log('is blockchain valid?',myungsCoin.isChainValid());
-
-
-//JSON.stringify(문자열로 변환할 값,null이면 모든속성을 포함,마지막 숫자는 space개수)
- console.log(JSON.stringify(myungsCoin,null,4));
