945c4a83725d4e7e5bf3e3a20026b20f3284d0fd
myungsworld <myungsworld@gmail.com>
2020-07-04 16:18:12 +0900

    implemented structure for better ts env

---
+++ typechain/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!!`;
+};
+console.log(sayHi("song", 24, "male"));
+//# sourceMappingURL=index.js.map
---
+++ typechain/src/index.ts
@@ -0,0 +1,9 @@
+
+const sayHi = (name:string, age:number, gender:string):string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!!`;
+};
+
+
+console.log(sayHi("song",24,"male"));
+
+export {};
