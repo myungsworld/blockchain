815182229816979da903bd4b1a2e5656fc8bd896
myungsworld <myungsworld@gmail.com>
2020-07-05 00:56:00 +0900

    class implemented

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,20 +1,21 @@
-interface Human{
-    name : string,
-    age : number,
-    gender : string
+class Human {
+    public name : string;
+    public age : number;
+    public gender : string;
+    constructor(name:string,age:number,gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name : "song",
-    age : 27,
-    gender : "male"
-};
+const song = new Human("song",27,"male")
 
 const sayHi = (person:Human) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
 };
 
 
-console.log(sayHi(person));
+console.log(sayHi(song));
 
 export {};
