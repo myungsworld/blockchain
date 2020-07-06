cf9efc2aabaf3c33478ccc8fbc8e0ed25e70d8bb
myungsworld <myungsworld@gmail.com>
2020-07-06 18:01:22 +0900

    updated typechain

--- typechain/dist/index.js
+++ typechain/dist/index.js
@@ -1,15 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const song = new Human("song", 27, "male");
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
-};
-console.log(sayHi(song));
+const genesisBlock = new Block(0, "23232323", "", "hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,21 +1,20 @@
-class Human {
-    public name : string;
-    public age : number;
-    public gender : string;
-    constructor(name:string,age:number,gender:string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index : number;
+    public hash: string;
+    public previousHash : string;
+    public data : string;
+    public timestamp : number;
+    constructor(index:number,hash:string,previousHash:string,data:string,timestamp:number){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const song = new Human("song",27,"male")
+const genesisBlock : Block = new Block(0,"23232323", "", "hello", 123456);
 
-const sayHi = (person:Human) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
-};
+let blockchain : [Block] =[genesisBlock];
 
-
-console.log(sayHi(song));
-
-export {};
+console.log(blockchain);
