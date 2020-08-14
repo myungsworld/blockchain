f521734d2c5b2d13e1a7209086a430103d1a18ef
myungsworld <56465854+myungsworld@users.noreply.github.com>
2020-08-14 11:55:05 +0900

    Update marshal.go

--- go/src/marshal/marshal.go
+++ go/src/marshal/marshal.go
@@ -5,8 +5,34 @@ import (
 	"fmt"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type User2 struct {
+	// 미출력
+	Name string `json:"-"`
+	// 문자열로 출력
+	Ages int `json:"age, string"`
+	// 값이 비어있으면 미출력
+	Phone string `json:omitempty`
+}
+
 func main() {
-	b, _ := json.Marshal(true)
-	fmt.Println(b)
-	fmt.Println(string(b))
+	//var u = User{"Go", 7}
+	//b, _ := json.Marshal(u)
+
+	//fmt.Println(b)
+	//fmt.Println(string(b))
+
+	var u2 = User2{"song", 10, ""}
+	b2, _ := json.Marshal(u2)
+	fmt.Println(b2)
+	fmt.Println(string(b2))
+
+	var s = `{"name":"song","age":27}`
+	var u User
+	json.Unmarshal([]byte(s), &u)
+	fmt.Printf("%+v\n", u)
 }
